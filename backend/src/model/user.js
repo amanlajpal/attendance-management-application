fa373dc437ce61078b86eb44dff8883188bfb135
Aman Lajpal <42869088+amanlajpal@users.noreply.github.com>
2026-01-04 20:04:43 +0530

    feat: refactor user creation logic to use async function for better error handling

--- backend/src/model/user.js
+++ backend/src/model/user.js
@@ -1,15 +1,27 @@
 import client from "../connection/pgdb.js";
 
-client.query(`
-        CREATE EXTENSION IF NOT EXISTS CITEXT;
-    `)
+async function createUserAndRelatedExtension() {
+    await client.query(`
+            CREATE EXTENSION IF NOT EXISTS CITEXT;
+        `)
 
-client.query(`
-        CREATE TABLE IF NOT EXISTS users
-        (
-            id SERIAL PRIMARY KEY,
-            name VARCHAR NOT NULL,
-            email CITEXT UNIQUE NOT NULL,
-            password VARCHAR NOT NULL
-        );
-    `)
+    await client.query(`
+            CREATE TABLE IF NOT EXISTS users
+            (
+                id SERIAL PRIMARY KEY,
+                name VARCHAR NOT NULL,
+                email CITEXT UNIQUE NOT NULL,
+                password VARCHAR NOT NULL
+            );
+        `)
+}
+
+createUserAndRelatedExtension()
+    .then(() => {
+        console.log("Table and extension created successfully");
+    }).catch((err) => {
+        console.error(err);
+    }).finally(async () => {
+        await client.end();
+    })
+
