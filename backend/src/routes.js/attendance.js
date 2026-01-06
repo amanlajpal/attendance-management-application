3b44a485693d9bc7184dda11bb78d52e34335e17
Aman Lajpal <42869088+amanlajpal@users.noreply.github.com>
2026-01-06 19:22:39 +0530

    feat: added attendance update route

--- backend/src/routes.js/attendance.js
+++ backend/src/routes.js/attendance.js
@@ -29,6 +29,33 @@ attendanceRouter.post("/create", async (req, res, next) => {
     }
 })
 
+attendanceRouter.put("/update", async (req, res, next) => {
+
+    console.log(req.body);
+    const { attendee_id, attendance, comment } = req.body;
+
+    if (!attendee_id || !attendance) {
+        res.status(404).send({
+            message: "Attendee Id and attendance both are mandatory!"
+        })
+    }
+
+    const attendanceFound = (await client.query("SELECT * FROM attendance WHERE date_trunc('day', created_at) = CURRENT_DATE;"))?.rows?.[0];
+
+    if (attendanceFound) {
+        const updatedAttendance = (await client.query("UPDATE attendance SET attendee_id = $1, attendance = $2, comment = $3 WHERE id = $4 RETURNING *;", [attendee_id, attendance, comment, attendanceFound.id])).rows[0];
+
+        res.status(200).send({
+            message: "Attendance updated successfully",
+            data: updatedAttendance
+        })
+    } else {
+        res.status(404).send({
+            message: "Attendance not found",
+        })
+    }
+})
+
 
 
 export default attendanceRouter;
