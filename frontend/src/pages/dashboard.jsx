22da44da525e1a454032f85f1b46df621d6987c0
Aman Lajpal <42869088+amanlajpal@users.noreply.github.com>
2026-01-15 16:43:15 +0530

    style: enhance dashboard layout and improve header styling

--- frontend/src/pages/dashboard.jsx
+++ frontend/src/pages/dashboard.jsx
@@ -9,21 +9,19 @@ export default function Dashboard() {
 
   return (
     <div className="bg-gray-100 h-screen w-full">
-      <div className="w-full h-1/12 bg-white">
-        <div className="w-full h-full flex justify-center items-center">
-          <h1>Attendance Management Application</h1>
-        </div>
-      </div>
-      <div className="h-11/12 flex">
-        <div className="w-1/5 h-full bg-white px-5 py-3 flex flex-col justify-between">
+      <div className="h-full flex">
+        <div className="w-1/5 h-full bg-white px-5 py-5 flex flex-col justify-between">
           <div>
-            <div className="w-full h-10 hover:bg-gray-300 bg-gray-200 rounded-xl px-2 py-1 flex items-center justify-left gap-5">
+            <div className="w-full h-25 flex justify-center items-center bg-purple-50 rounded hover:bg-purple-100">
+              <h1 className="text-center font-bold">Attendance Management Application</h1>
+            </div>
+            <div className="w-full h-10 hover:bg-gray-300 bg-gray-200 rounded-xl px-2 py-1 my-5 flex items-center justify-left gap-5">
               <HowToRegIcon />
               <p className="text-sm">Attendance</p>
             </div>
           </div>
           <div className="flex flex-col gap-2">
-            <div className="w-full h-10 rounded-xl px-2 py-1 flex items-center justify-left gap-5">
+            <div className="w-full h-10 rounded-xl px-2 py-1 flex items-center justify-left gap-5 hover:bg-gray-200">
               <AccountCircleIcon />
               Hi {user?.name || "User"}
             </div>
